358e2e22078fce4ede7857c1bec2130a987c3221
ahmed abdelqodous <ml.ahmed1187@gmail.com>
2025-09-11 15:06:30 +0300

    fixit initializing

---
+++ fixit-api/src/test/java/kw/yourexp/fixit_api/FixitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kw.yourexp.fixit_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
